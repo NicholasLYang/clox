be902bebb4a0f719578b96ea8be49c4a348fa967
Nicholas Yang <ny585@nyu.edu>
2018-02-27 15:09:16 -0500

    added rest of chapter 14. Got the constants working

--- debug.h
+++ debug.h
@@ -5,5 +5,7 @@
 
 void disassembleChunk(Chunk* chunk, const char* name);
 int diassembleInstruction(Chunk* chunk, int i);
+static int constantInstruction(const char* name, Chunk* chunk,
+			       int offset);
 
 #endif
